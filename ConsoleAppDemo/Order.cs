c710547c7ee8f2889fc722608fcb94a9a060526d
YuriiTrofimenko <tyaa@ukr.net>
2021-06-28 15:51:25 +0300

    structures

--- ConsoleAppDemo/Order.cs
+++ ConsoleAppDemo/Order.cs
@@ -1,18 +1,44 @@
 namespace ConsoleAppDemo
 {
-    public class Order
+    public struct Order
+    // public class Order
     {
         private static int lastId;
-        public int Id { get; }
 
-        public Order()
+        public delegate void PrintInfoDelegate();
+        public int Id { get; }
+        public int UserId { get; }
+        public int ProductId { get; }
+        public int Quantity { get; }
+        public decimal Price { get; }
+
+        /* public Order()
         {
             Id = ++lastId;
+        } */
+
+        public Order(int userId, int productId, int quantity, decimal price): this()
+        {
+            Id = ++lastId;
+            UserId = userId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
         }
 
         /* public int getId()
         {
             return id;
         } */
+
+        public void PrintInfo(PrintInfoDelegate printer)
+        {
+            printer.Invoke();
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}, Price = {Price}, Quantity = {Quantity}";
+        }
     }
 }
--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -11,9 +11,19 @@ namespace ConsoleAppDemo
             // Console.WriteLine(new Order().getId());
             // Console.WriteLine(new Order().getId());
             // new Order().Id = 999; // Forbidden!
-            Console.WriteLine(new Order().Id);
-            Console.WriteLine(new Order().Id);
-            Console.WriteLine(new Order().Id);
+            // Console.WriteLine(new Order().Id);
+            // Console.WriteLine(new Order().Id);
+            // Console.WriteLine(new Order().Id);
+            
+            // Console.WriteLine(new Order());
+            // Console.WriteLine(new Order());
+            // Console.WriteLine(new Order());
+            
+            // Console.WriteLine(new Order(1, 2, 190, new decimal(99.09)));
+            // Console.WriteLine(new Order(1, 2, 190, new decimal(99.09)));
+            // Console.WriteLine(new Order(1, 2, 190, new decimal(99.09)));
+            
+            
         }
     }
 }
